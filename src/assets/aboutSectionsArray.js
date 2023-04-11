cdea673900aa7302bdb794483a2f917e3f93e6de
joshuahevelow <joshhevelow@comcast.net>
2023-04-10 22:59:44 -0400

    full frontend

---
+++ src/assets/aboutSectionsArray.js
@@ -0,0 +1,18 @@
+//IMPORTANT: scroll values in About.js must be adjust when this text is changed (ask Josh if confused)
+
+const aboutSectionsArray = [
+    "This project looks to provide users with access to mathematical resources on the web. This is done by utilizing " +
+    "semantic search, a machine learning technique utilizing the vector representation of words. With the Open AI " + 
+    "Embeddings API, we were able to gather the vector representation of the transcripts of Youtube’s math-related " + 
+    "content. We combine this search engine with a roadmap to indicate which topics should be learned next and a " + 
+    "calculator to help solve problems encountered along the way.",
+    "Our goal is to make the field of mathematics more accessible and understandable for students and anyone else that " +
+    "wants to learn more. Many people find themselves stuck on a certain math problem or topic. We want to make those " +
+    "tough problems less intimidating, and instead inspire people to improve their problem solving skills. By providing " +
+    "the specific time stamp of the concept being searched within a video, we hope to save users the time of clicking " +
+    "through videos and trying to find the exact point they need to see. From technology to finance to medicine to just "+
+    "about everything else, math is key skill. Hopefully, by using our site, you can learn to embrace it and learn "+
+    "something new everyday!"
+]
+
+export default aboutSectionsArray;
